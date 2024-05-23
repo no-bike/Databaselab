908e3b8f5a96f08dc1fc8978dd7f3926efc68c2a
no-bike <837606776@qq.com>
2024-05-23 15:32:01 +0800

    feat: RmScan::next

--- src/record/rm_scan.cpp
+++ src/record/rm_scan.cpp
@@ -20,6 +20,7 @@ RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
     // 初始化file_handle和rid（指向第一个存放了记录的位置）
     rid_.page_no = 0;
     rid_.slot_no = 0;
+    file_handle_ = file_handle;
     for (; rid_.page_no < file_handle->file_hdr_.num_pages; rid_.page_no++) {
         RmPageHandle page_handle = file_handle_->fetch_page_handle(rid_.page_no);
         // 遍历页面中的所有slot
@@ -43,7 +44,24 @@ void RmScan::next() {
     // Todo:
     // 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
 
+    // 遍历所有的页面
+    for (; rid_.page_no < file_handle_->file_hdr_.num_pages; rid_.page_no++) {
+        RmPageHandle page_handle = file_handle_->fetch_page_handle(rid_.page_no);
+        // 遍历页面中的所有slot
+        for (; rid_.slot_no < file_handle_->file_hdr_.num_records_per_page; rid_.slot_no++) {
+            // 如果找到一个非空闲的slot，返回
+            if (page_handle.bitmap[rid_.slot_no] == 1) {
+                return;
+            }
+        }
+        // 如果当前页面的所有slot都已经遍历完，那么遍历下一个页面的第一个slot
+        rid_.slot_no = 0;
+    }
+    // 如果所有的页面和slot都已经遍历完，那么设置rid_为无效值
+    rid_.page_no = -1;
+    rid_.slot_no = -1;
 }
+
 
 /**
  * @brief ​ 判断是否到达文件末尾
